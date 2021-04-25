22832761655a7cc8e5889750a47665e7957034bb
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-04-25 10:10:05 +0200

    First steps in OpenGL

---
+++ cQualizer.OpenGL/ApplicationWindow.cs
@@ -0,0 +1,51 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace cQualizer.OpenGL {
+
+	public class ApplicationWindow : GameWindow {
+
+		public ApplicationWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+		: base(gameWindowSettings, nativeWindowSettings) { }
+
+		protected override void OnLoad() {
+			base.OnLoad();
+		}
+
+		protected override void OnResize(ResizeEventArgs e) {
+			base.OnResize(e);
+
+			GL.Viewport(0, 0, Size.X, Size.Y);
+		}
+
+		private readonly float[] vertecies = new float[] {
+			-0.5f, -0.5f,
+			 0.5f, -0.5f,
+			 0.0f,  0.5f
+		};
+
+		protected override void OnRenderFrame(FrameEventArgs args) {
+			base.OnRenderFrame(args);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			int vertexArray = GL.GenVertexArray();
+			int vertexBuffer = GL.GenBuffer();
+
+			GL.BindVertexArray(vertexArray);
+			GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+			GL.BufferData(BufferTarget.ArrayBuffer, vertecies.Length * sizeof(float), vertecies, BufferUsageHint.StaticDraw);
+			GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 2 * sizeof(float), 0);
+			GL.EnableVertexAttribArray(0);
+
+			GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+			SwapBuffers();
+		}
+
+		protected override void OnUpdateFrame(FrameEventArgs args) {
+			base.OnUpdateFrame(args);
+		}
+	}
+}
--- cQualizer.OpenGL/Program.cs
+++ cQualizer.OpenGL/Program.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using System;
 
 namespace cQualizer.OpenGL {
+
 	class Program {
+
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+
+			var gameSettings = new GameWindowSettings();
+			var nativeSettings = new NativeWindowSettings();
+
+			gameSettings.IsMultiThreaded = false;
+			gameSettings.RenderFrequency = 0;
+
+			nativeSettings.IsFullscreen = false;
+			nativeSettings.StartFocused = true;
+			nativeSettings.NumberOfSamples = 0;
+			nativeSettings.Flags = ContextFlags.Debug;
+			nativeSettings.WindowBorder = WindowBorder.Resizable;
+			nativeSettings.Title = "cQualizer";
+
+			var window = new ApplicationWindow(gameSettings, nativeSettings);
+
+			window.Run();
 		}
 	}
 }
